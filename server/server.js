5a343c4449b01a6637c45eb714869e91d49a3db5
philipjpark <philipparknyc@gmail.com>
2023-10-23 14:08:21 -0400

    Specified endpoint for trips

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import tripRoutes from './routes/trips.js'
 
 // create express app
 const app = express()
@@ -11,6 +12,10 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
 });
+
+// app.use('/trips', tripRoutes)
+app.use('/api/trips', tripRoutes)
+
 
 // Start the server
 const PORT = process.env.PORT || 3001
